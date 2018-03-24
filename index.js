9ad0d14a31f59395725f19f7146b190d962f3a8e
EugeneFH <37742029+EugeneFH@users.noreply.github.com>
2018-03-24 17:23:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,22 +2,11 @@ console.log("Its okay");
 const Discord = require('discord.js');
 const FHbot = new Discord.Client();
 
-FHbot.login("NDI1OTQ1NDE0ODMxOTY0MTgz.DZO0Gw.DHmdr2ypUfJUkksMamVaRfSeISQ");
-
 FHbot.on('message', (message)=>{
     if(message.content == "!FHhelp") {
         message.delete();
         message.reply("Полезная информация о сервере: \n Наша группа ВК: <https://vk.com/flaminghalo> \n Наш Discord: <https://discord.gg/E4X6nhF> \n Сайт проекта: <http://flaminghalo.ru> \n Форум: <http://forum.flaminghalo.ru> \n Скоро открытие, следите за новостями :D");
     }
 });
-
-function func() {
-    FHbot.on('message', (message)=>{
-        if(message.content == "!FHavatar") {
-            message.delete();
-            message.author.avatarURL();
-        }
-    })
-};
 
 setTimeout(func, 1000);
